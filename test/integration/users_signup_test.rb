1fa63ff2366129b300e1935f5b108171cf95b677
laserylee <laserylee2013@gmail.com>
2019-05-16 10:56:54 +0800

    Finish 7.4.4, User signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,5 +13,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
+    :q
+    :q
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post users_path, params: { user: { name: "test",
+                                         email: "test@test.test",
+                                         password: "testing",
+                                         password_confirmation: "testing" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not !flash.key?(:success)
   end
 end
