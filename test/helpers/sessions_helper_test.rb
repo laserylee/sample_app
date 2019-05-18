9403fac9526cdddfdd7c941b82cf9e5c81ac2830
laserylee <laserylee2013@gmail.com>
2019-05-18 18:12:46 +0800

    Corrected typo and finished SessionsHelperTest

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -2,23 +2,19 @@ require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
 
-  def Setup
+  def setup
     @user = users(:michael)
     remember(@user)
   end
 
-# test "current_user returns right user when session is nil" do
-#   assert_equal @user, current_user
-#   assert is_logged_in?
-# end
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
 
-# test "current_user returns nil when remember digest is wrong" do
-#   @user.update_attribute(:remember_digest, User.digest(User.new_token))
-#   assert_nil current_user
-# end
-  
-  test "unfunction fixture" do
-    assert_nil @user
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
   end
 
 end
