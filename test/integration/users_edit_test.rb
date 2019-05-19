633fe91bb470736367aeec54aa1f2a00ce1fe76d
laserylee <laserylee2013@gmail.com>
2019-05-19 19:56:30 +0800

    COntinue to 10.3 after corrected user edit integration test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -40,7 +40,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
-    email = "foobar@.com"
+    email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
                                               email: email,
                                               password: "",
