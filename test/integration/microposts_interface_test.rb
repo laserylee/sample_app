9169e30447b13c0a0991062b340872251b06d293
laserylee <laserylee2013@gmail.com>
2019-05-29 23:14:47 +0800

    Added image uploader and tester, havent format the size yet

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid Submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,11 +18,13 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid Submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content} }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
     assert_redirected_to root_url
     follow_redirect!
+    assert @user.microposts.find_by(content:content).picture?
     assert_match content, response.body
     # Delete post
     assert_select 'a', text: 'delete'
